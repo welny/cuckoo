f5eb1d0c2513e9320e9ce787cd257e466a1d3cd3
Marcin Sikora <marcin.sq5pgf@gmail.com>
2015-10-06 09:40:42 +0200

    wersja inicjalna

---
+++ README.md
@@ -0,0 +1,1 @@
+Cuckoo push notifications server
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+requires = [
+      'requests'
+    ]
+
+setup(name='Cockoo',
+      version='0.5.0',
+      description='Cuckoo Apple push notifications server',
+      long_description=README,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Autobahn",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Biokod Lab Sp. z o.o.',
+      author_email='biuro@biokod.pl',
+      url='',
+      keywords='web wsgi bfg application',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='cuckoo-test',
+      install_requires=requires,
+      )
