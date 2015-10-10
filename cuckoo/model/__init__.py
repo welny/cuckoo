c15dc070d45e1f54985bc825e8e247fbf02e2458
Marcin Sikora <marcin.sq5pgf@gmail.com>
2015-10-10 21:23:24 +0200

    niepotrzebne importy

--- cuckoo/model/__init__.py
+++ cuckoo/model/__init__.py
@@ -1,7 +1,1 @@
 # package
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-from zope.sqlalchemy import ZopeTransactionExtension
-
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-Base = declarative_base()
