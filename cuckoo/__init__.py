1b9462408f4d215516dfc22a8b73ffeb1b21a179
Aleksandra Suchanecka <ola99suchanecka@gmail.com>
2015-10-21 15:24:28 +0200

    poprawka

--- cuckoo/__init__.py
+++ cuckoo/__init__.py
@@ -1,2 +1,2 @@
 from cuckoo.model.connections import APNService
-from cuckoo.model.messages import Payload, PayloadAlert, Frame
+from cuckoo.model.messages import Payload, PayloadAlert, Frame, GCMMessage
